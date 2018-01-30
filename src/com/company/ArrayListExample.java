365d8ac6a23121b505e89e55655c5f12993f5e0d
gagankhalsa <gagankhalsa@gmail.com>
2018-01-29 22:29:13 -0500

    Added methods to arrayList

--- src/com/company/ArrayListExample.java
+++ src/com/company/ArrayListExample.java
@@ -3,6 +3,8 @@ package com.company;
 import com.sun.org.apache.xpath.internal.SourceTree;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class ArrayListExample {
     public static void main(String[] args) {
@@ -18,5 +20,21 @@ public class ArrayListExample {
         System.out.println("list is"+obj);
         obj.remove("jot");
         System.out.println("list is"+obj+"size of array list"+obj.size());
+        ArrayList<Integer> loop=new ArrayList<Integer>();
+        loop.add(0,20);
+        loop.add(1,30);
+        loop.add(2,3);
+        System.out.println(loop);
+        for (Integer num:loop
+             ) {
+            System.out.println(num);
+        }
+        Collections.sort(loop);
+        System.out.println("Iterator example");
+        Iterator iterator=loop.iterator();
+        while (iterator.hasNext())
+        {
+            System.out.println(iterator.next());
+        }
     }
 }
